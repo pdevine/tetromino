a7f882e1c39174c317b13ae0b9a5238851896f50
Patrick Devine <pdevine@XX-MBP13-2017.local>
2018-10-16 18:10:34 -0700

    Add copyright

--- title.go
+++ title.go
@@ -25,6 +25,10 @@ const logo = `[][][] [][] [][][] [][]     []   []    [] [] []   []   []
 const elek_costume1 = "e l e k t r o n i k a"
 const elek_costume2 = "Электроника"
 
+const copyright1 = "(c) 2018 Patrick Devine\n   patrick@immense.ly"
+
+const instructions1 = "Press any key to continue"
+
 func NewTitle() *Title {
 	s := &Title{BaseSprite: sprite.BaseSprite{
 		X:       17,
@@ -47,7 +51,12 @@ func (s *Title) Update() {
 		if s.Y < s.TargetY {
 			s.Y += s.VY
 		} else {
-			s.state = resting
+			if s.state != resting {
+				s.state = resting
+				for _, spr := range []*sprite.BaseSprite{NewCopyright(), NewInstructions()} {
+					allSprites.Sprites = append(allSprites.Sprites, spr)
+				}
+			}
 		}
 	}
 }
@@ -79,4 +88,26 @@ func (s *TitleString) Update() {
 		s.NextCostume()
 		s.Timer = 0
 	}
+}
+
+func NewCopyright() *sprite.BaseSprite {
+	s := &sprite.BaseSprite{
+		X:	37,
+		Y:	14,
+		Visible: true,
+	}
+
+	s.AddCostume(sprite.NewCostume(copyright1, '!'))
+	return s
+}
+
+func NewInstructions() *sprite.BaseSprite {
+	s := &sprite.BaseSprite{
+		X: 	 36,
+		Y:	 23,
+		Visible: true,
+	}
+
+	s.AddCostume(sprite.NewCostume(instructions1, '!'))
+	return s
 }
